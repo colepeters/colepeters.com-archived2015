80673a434df3ea4c2647efb321043875ec25e3f8
Cole Peters <cole@colepeters.com>
2013-09-22 23:04:35 +0100

    Take that FOUC and shove it!
    Signed-off-by: Cole Peters <cole@colepeters.com>

--- app/assets/javascripts/animations.js
+++ app/assets/javascripts/animations.js
@@ -1,5 +1,7 @@
-// The site menu trigger
+// Controls FOUC prevention (wait) and site menu trigger
 $(function(){
+    $('.wait').delay('slow').removeClass('wait');
+
     $('a.open_sesame').click(function(){
         $('.menu').fadeIn();
     });
