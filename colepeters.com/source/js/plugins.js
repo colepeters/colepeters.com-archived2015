5b604100a873200566feb38708c5019c0df032af
Cole Peters <cole@colepeters.com>
2013-12-05 15:08:36 +0000

    Update plugins js - import menu js from prev

--- colepeters.com/source/js/plugins.js
+++ colepeters.com/source/js/plugins.js
@@ -11,4 +11,14 @@ if (!(window.console && console.log)) {
     }());
 }
 
-// Place any jQuery/helper plugins in here.
+// Controls FOUC prevention (wait) and site menu trigger
+$(function(){
+    $('.wait').delay('slow').removeClass('wait');
+
+    $('a.open_sesame').click(function(){
+        $('.menu').fadeIn();
+    });
+    $('a.shut_yo_mouth').click(function(){
+        $('.menu').fadeOut();
+    });
+});
