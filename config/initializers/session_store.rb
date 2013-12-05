e0d9209c4c1a994234c4133acf6132f28901db5f
Cole Peters <cole@colepeters.com>
2013-12-05 14:38:56 +0000

    Remove most of the Rails stuff
    Signed-off-by: Cole Peters <cole@colepeters.com>

--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-ColepetersCom::Application.config.session_store :cookie_store, key: '_colepeters_com_session'
--- test/helpers/artefacts_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ArtefactsHelperTest < ActionView::TestCase
-end
