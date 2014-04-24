8680f307bec57c0089a10d63208d51a54fd88373
Cole Peters <cole@colepeters.com>
2014-04-24 14:43:25 -0500

    Ha ha ha ok ready nao

--- Gruntfile.js
+++ Gruntfile.js
@@ -295,7 +295,8 @@ module.exports = function (grunt) {
                         '.htaccess',
                         'images/{,*/}*.webp',
                         '{,*/}*.html',
-                        'styles/fonts/{,*/}*.*'
+                        'styles/fonts/{,*/}*.*',
+                        'fonts/{,*/}*.*'
                     ]
                 }]
             },
