dfb5b33254b16d48804187372f5c71516b2b6b6b
Cole Peters <cole@colepeters.com>
2015-02-05 18:27:00 +0000

    Let's get back on

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ colepeters.com
 ==============
 
 This is the repo for [my personal website](https://colepeters.com).
+Note that contents of the `src/fonts` directory are not tracked and will need to be downloaded seperately.
---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var connect = require('gulp-connect');
+var del = require('del');
+
+gulp.task('clean', function() {
+  del.sync(['build/**']);
+});
+
+gulp.task('html', function() {
+  gulp.src('./src/*.html')
+    .pipe(gulp.dest('./build'))
+    .pipe(connect.reload());
+});
+
+gulp.task('styles', function() {
+  gulp.src('./src/sass/styles.sass')
+    .pipe(sass({ style: 'expanded', indentedSyntax: true }))
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(rename({ suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('fonts', function() {
+  gulp.src('./src/fonts/**')
+    .pipe(gulp.dest('./build/fonts'));
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'build',
+    livereload: true
+  });
+});
+
+gulp.task('default', ['clean', 'html', 'fonts', 'styles', 'connect'], function() {
+  gulp.watch('./src/*.html', ['html']);
+  gulp.watch('./src/sass/**', ['styles']);
+});
